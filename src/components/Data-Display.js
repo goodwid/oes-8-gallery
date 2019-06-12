fec7369ca36340d7e8bbbb08e7002636af7e83e2
goodwid <goodwid@gmail.com>
2019-06-12 12:14:01 -0700

    Updated to 2019 images

--- src/components/Data-Display.js
+++ src/components/Data-Display.js
@@ -9,15 +9,23 @@ export default class DataDisplay extends Component {
 
   render() {
     const { image } = this.props;
-    const imageRoot = 'https://s3-us-west-2.amazonaws.com/oes-8-gallery/';
+    const { artist, title, view, media } = image;
+    const imageRoot = 'https://s3-us-west-2.amazonaws.com/oes-8-gallery/2019/';
+    // const imageRoot = './2019-images/';
     const backgroundImage = imageRoot + image.src;
     const style = {
       backgroundImage: `url("${backgroundImage}")`
     };
-  
+    let footerText = '';
+    if (artist) footerText += `${artist}`;
+    if (title) footerText += ` - ${title}`;
+    if (view) footerText += ` - ${view}`;
+    if (media) footerText += ` - ${media}`;
+    
+
     return (
       <div className={styles.data} style={style}>
-        <Footer text={`${image.artist} - ${image.title}`} />
+        <Footer text={footerText} />
       </div>
     );
   }
